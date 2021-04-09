c65028febf0364982d8cf595849034fb12c2f9fd
Kasper Hesse <47347446+KasperHesse@users.noreply.github.com>
2021-04-09 09:07:53 +0200

    Update config files

--- src/main/scala/utils/Config.scala
+++ src/main/scala/utils/Config.scala
@@ -12,7 +12,7 @@ object Config {
   /** What type of multiplier to use if nothing is explicitly stated when instantiating */
   val MULTYPE = MulTypes.SINGLECYCLE
   /** How many stages to use in Newton-Raphson divisors stage 3 */
-  val NRDIV_STAGE3_REPS = 3
+  var NRDIV_STAGE3_REPS = 3
 
   //Compile-time constant definitions
   /** Number of element in the x-direction */
@@ -31,13 +31,19 @@ object Config {
   /** Number of nodes in the z-direction */
   val NZ = NELZ + 1
 
+  /** Total number of elements in the design domain */
+  val NELEM = NELX*NELY*NELZ
   /** Total number of element DOF in the design domain */
-  val NDOF = 3*NX*NY*NZ
+  var NDOF = 3 * NX * NY * NZ
 
   val WORD_LENGTH = Fixed.FIXED_WIDTH
 
   /** The number of element in the vector register file */
   var NUM_VECTOR_REGISTERS = 32
+  /** The number of vector register slots that are adressible from instructions */
+  var NUM_VREG_SLOTS = 4
+  /** The number of X-value registers in the x register file. This value is always equal to [[NUM_VREG_SLOTS]] */
+  var NUM_X_REG = NUM_VREG_SLOTS
   /** The number of values stored in each entry in the vector register file */
   var VECTOR_REGISTER_DEPTH = 24
   /** The number of elements in the scalar register file */
@@ -45,19 +51,32 @@ object Config {
   /** The number of processing elements used in the design. Determines the width of vectors carrying values between modules. */
   var NUM_PROCELEM = 8
   /** The number of subvectors in each vector register */
-//  val NUM_SUBVECTORS = VECTOR_REGISTER_DEPTH/NUM_PROCELEM
-  /** The number of vector register slots that are adressible from instructions */
-  var NUM_VREG_SLOTS = 4
-  /** The width (in registers) of each vector register slot */
-//  val VREG_SLOT_WIDTH = NUM_VECTOR_REGISTERS/NUM_VREG_SLOTS
-
+  var SUBVECTORS_PER_VREG = VECTOR_REGISTER_DEPTH/NUM_PROCELEM
+  /** The width (number of registers) of each vector register slot */
+  var VREG_SLOT_WIDTH = NUM_VECTOR_REGISTERS/NUM_VREG_SLOTS
+  /** The width and height of the KE matrix */
+  var KE_SIZE = 24
+  /** The bitwidth of an instruction */
+  var INSTRUCTION_WIDTH = 32
   /** Simulation flag. Assert inside of a tester to use simulation-specific functionality */
   var SIMULATION = false
 
-  require(NUM_VECTOR_REGISTERS >= NUM_VREG_SLOTS, "Must have more vector registers than register slots")
-  require(NUM_VECTOR_REGISTERS % NUM_VREG_SLOTS == 0, "Number of vector registers must me a multiple of register slots")
+  checkRequirements()
 
-  require(NRDIV_STAGE3_REPS >= 1, "Newton-Raphson division requires at least one iteration in stage 3")
+  /** Checks if all configurations requirements are held. This *must* be called in a tester if config values are changed */
+  def checkRequirements() {
+    SUBVECTORS_PER_VREG = VECTOR_REGISTER_DEPTH/NUM_PROCELEM
+    VREG_SLOT_WIDTH = NUM_VECTOR_REGISTERS/NUM_VREG_SLOTS
+    NUM_X_REG = NUM_VREG_SLOTS
+
+    require(NUM_VECTOR_REGISTERS >= NUM_VREG_SLOTS, "Must have more vector registers than register slots")
+    require(NUM_VECTOR_REGISTERS % NUM_VREG_SLOTS == 0, "Number of vector registers must me a multiple of register slots")
+    require(KE_SIZE == VECTOR_REGISTER_DEPTH, "KE_SIZE must equal VECTOR_REGISTER_DEPTH for proper matrix-vector products")
+    require(NRDIV_STAGE3_REPS >= 1, "Newton-Raphson division requires at least one iteration in stage 3")
+    require(NUM_X_REG == NUM_VREG_SLOTS, "NUM_X_REG must equal NUM_VREG_SLOTS for easier access")
+    require(NUM_PROCELEM == VREG_SLOT_WIDTH, "NUM_PROCELEM and VREG_SLOT_WIDTH must be the same")
+  }
+
 }
 
 object DivTypes extends Enumeration {
--- src/main/scala/utils/Fixed.scala
+++ src/main/scala/utils/Fixed.scala
@@ -8,11 +8,13 @@ import chisel3._
 object Fixed {
   //Default config: Fracwidth 48, intwidth 15, fixedwidth 64
   /** Number of bits in the fractional part of a fixed-point number */
-  val FRAC_WIDTH = 38
+  var FRAC_WIDTH = 38
   /** Number of bits in the integer part of a fixed-point number */
-  val INT_WIDTH = 15
+  var INT_WIDTH = 15
   /** Number of bits total that a fixed-point number takes up */
-  val FIXED_WIDTH = 54
+  var FIXED_WIDTH = 54
+
+  require(FRAC_WIDTH+INT_WIDTH == FIXED_WIDTH-1, "Width of Fixed-point numbers must be 1(sign) + frac_width+int_width")
 
   /**
    * Converts a given double value into its corresponding fixed-point representation
@@ -32,7 +34,7 @@ object Fixed {
    * @param value The number to convert
    * @return A double with the value that the fixed-point number represents
    */
-  def sint2double(value: SInt): Double = {
+  def fixed2double(value: SInt): Double = {
     value.litValue().toDouble * math.pow(2,-FRAC_WIDTH)
   }
 
@@ -43,6 +45,24 @@ object Fixed {
    */
   def fixed2double(value: Long): Double = {
     value.toDouble * math.pow(2,-FRAC_WIDTH)
+  }
+
+  /**
+   * Convert an SInt to the long literal that its interger part represents
+   * @param value The SInt to convert
+   * @return A long representing the same integer part
+   */
+  def fixed2long(value: SInt): Long = {
+    value.litValue.toLong >> FRAC_WIDTH
+  }
+
+  /**
+   * Converts a long literal to an SInt with the same value
+   * @param value The long to convert
+   * @return An SInt representing the same value
+   */
+  def long2fixed(value: Long): SInt = {
+    (value << FRAC_WIDTH).S(FIXED_WIDTH.W)
   }
 
   /**
@@ -70,7 +90,10 @@ object Fixed {
     } else {
       e
     }
+  }
 
+  def fixedAdd(x: SInt, y: SInt): SInt = {
+    fixedAdd(x.litValue().toLong, y.litValue.toLong).S
   }
 
   /**
@@ -100,11 +123,23 @@ object Fixed {
       e
     }
   }
+
+  /**
+   * Subtracts two SInts representing fixed-point numbers,
+   * the way the hardware would (within range [-1*2^INT_WIDTH^ ;2^INT_WIDTH^-1])
+   * @param x The first number
+   * @param y The second number
+   * @return a-b, within the limits of the addition hardware
+   */
+  def fixedSub(x: SInt, y: SInt): SInt = {
+    fixedSub(x.litValue().toLong, y.litValue.toLong).S(FIXED_WIDTH.W)
+  }
+
   /**
    * Multiply two doubles the way the hardware would (within range [-1*2^INT_WIDTH^ ;2^INT_WIDTH^-1])
    * @param x The first number
    * @param y The second number
-   * @return a*b, within in the limits of the addition hardware
+   * @return a*b, within in the limits of the hardware
    *
    */
   def fixedMul(x: Double, y: Double): Double = {
@@ -116,7 +151,7 @@ object Fixed {
    * the way the hardware would (within range [-1*2^INT_WIDTH^ ;2^INT_WIDTH^-1])
    * @param x The first number
    * @param y The second number
-   * @return a*b, within in the limits of the addition hardware
+   * @return a*b, within in the limits of the hardware
    *
    */
   def fixedMul(x: Long, y: Long): Long = {
@@ -134,6 +169,80 @@ object Fixed {
     }
   }
 
+  /**
+   * Multiply two SInts representing fixed-point numbers
+   * the way the hardware would (within range [-1*2^INT_WIDTH^ ;2^INT_WIDTH^-1])
+   * @param x The first number
+   * @param y The second number
+   * @return a*b, within in the limits of the hardware
+   */
+  def fixedMul(x: SInt, y: SInt): SInt = {
+    fixedMul(x.litValue().toLong, y.litValue.toLong).S
+  }
+
+  /**
+   * Divides two SInts the way the hardware would (to some degree of precision, at least)
+   * @param n The numerator
+   * @param d The denominator
+   * @return the fixed-point number representing n/d
+   */
+  def fixedDiv(n: SInt, d: SInt): SInt = {
+    double2fixed(fixed2double(n) / fixed2double(d)).S(FIXED_WIDTH.W)
+//    (fixed2long(n)/fixed2long(d)).S(FIXED_WIDTH.W)
+  }
+
+  /**
+   * Computes the fraction n/d by using the newton-raphson division method
+   * @param numer Numerator
+   * @param denom Denominator
+   * @return The quotient n/d, expressed as a long
+   */
+  def nrDiv(numer: Long, denom: Long): Long = {
+    //Stage 1: Count leading zeros
+    //Scale denominator between 0.5 and 1, scale numerator by same amount
+    var cntZeros = 0
+    var dd = if(denom < 0) ~denom + 1 else denom //Properly handle two's complement
+    var nn = numer
+    if(FIXED_WIDTH < 64) {
+      dd = dd << (64-FIXED_WIDTH)
+    }
+//    while((dd & ((BigInt(1) << FIXED_WIDTH-1)).toLong) == 0) {
+    while((dd & 0x8000000000000000L) == 0) {
+      dd = dd << 1
+      cntZeros = cntZeros + 1
+    }
+    //Difference from 16 spaces
+    val diff = INT_WIDTH - cntZeros
+    if(diff > 0) { //We must right-shift
+      dd = denom >> diff
+      nn = numer >> diff
+    } else if (diff < 0) { //We must left-shift
+      //Important: If left-shifting, we must preserve the sign bit of the original value. Only left-shift the 63 bits that make up the number?
+      //Alternatively, work with BigInts from here on?
+      dd = denom << -1*diff
+      nn = ((numer << -1*diff) & 0x7fffffffffffffffL) | (numer & 0x8000000000000000L)
+    } else {
+      dd = denom
+      nn = numer
+    }
+    //Problem at this stage: If left-shifting, we may severely mess up our value of the numerator
+    print(s"Original d: ${fixed2double(denom)}, scaled d: ${fixed2double(dd)}\n")
+    print(s"Original n: ${fixed2double(numer)}, scaled n: ${fixed2double(nn)}\n")
+    //Firstly. This only works if d is positive. If negative, invert it and then re-invert the result
+
+    //Stage 2: Calculate the value 48/17-32/17*D'
+    if(denom < 0) dd = ~dd + 1
+    var X = fixedSub(double2fixed(48.0/17.0),fixedMul(double2fixed(32.0/17.0),dd))
+    for(i <- 0 until 4) {
+      X = fixedAdd(X,fixedMul(X, fixedSub(double2fixed(1), fixedMul(dd,X))))
+    }
+    val reslong = fixedMul(nn, X) * (if(denom < 0) -1 else 1)
+    val res = fixed2double(reslong)
+    print(s"NR-div with fixeds gives $numer/$denom=$res\n")
+//    print(s"Deviation: ${n/d-res}\n")
+    reslong
+  }
+
 
 
   /**
@@ -148,9 +257,5 @@ object Fixed {
     if(gen == math.pow(2,INT_WIDTH)) {
       gen - math.pow(2,-FRAC_WIDTH)
     } else gen
-  }
-
-  if(FRAC_WIDTH+INT_WIDTH != FIXED_WIDTH-1) {
-    throw new IllegalArgumentException("Width of Fixed-point numbers must be 1(sign) + frac_width+int_width")
   }
 }
