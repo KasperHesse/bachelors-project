2934bfe1466b73c6f94ab2264d8ec80fa5a41b20
Kasper Hesse <47347446+KasperHesse@users.noreply.github.com>
2021-04-28 16:39:11 +0200

    Started work on instruction fetch

--- src/main/scala/pipeline/Fetch.scala
+++ src/main/scala/pipeline/Fetch.scala
@@ -1,5 +1,25 @@
 package pipeline
 
-class Fetch {
+import chisel3._
+import chisel3.util.experimental.loadMemoryFromFile
+import utils.Config._
+import firrtl.annotations._
 
+class FetchIO extends Bundle {
+  val id = new IfIdIO
+//  val addr = Input(UInt(4.W))
+}
+
+class Fetch(memsize: Int = 1024, memfile: String = "") extends Module {
+  val io = IO(new FetchIO)
+
+  val IP = RegInit(0.U(32.W))
+  IP := IP + 4.U
+
+  val imem = Mem(memsize, UInt(32.W))
+  if(SIMULATION) {
+    require(!memfile.isEmpty, "Cannot simulate without memory init")
+    loadMemoryFromFile(imem, memfile)
+  }
+  io.id.instr := imem((IP >> 2).asUInt())
 }
--- src/test/scala/pipeline/FetchSpec.scala
+++ src/test/scala/pipeline/FetchSpec.scala
@@ -1,5 +1,40 @@
 package pipeline
 
-class FetchSpec {
+import chisel3._
+import chiseltest._
+import org.scalatest._
+import org.scalatest.Assertions._
+import utils.Fixed._
 
+import scala.io.Source
+import utils.Config._
+
+class FetchSpec  extends FlatSpec with ChiselScalatestTester with Matchers  {
+
+  "Fetch stage" should "load a hex memory file" in {
+    val memfile = "src/test/resources/meminit/mem1.txt"
+    SIMULATION = true
+    test(new Fetch(memsize=8, memfile)) {dut =>
+      val lines = Source.fromFile(memfile)
+      var i = 0
+      for(line <- lines.getLines()) {
+//        dut.io.addr.poke(i.U)
+        val myint = Integer.parseInt(line, 16)
+        dut.io.id.instr.expect(myint.U)
+        dut.clock.step()
+        i += 1
+      }
+      lines.close()
+    }
+  }
+
+  "Fetch stage" should "throw an error if file is empty" in {
+    SIMULATION = true
+    try {
+      new Fetch()
+    } catch {
+      case a: IllegalArgumentException => assert(true)
+      case _: Throwable => assert(false)
+    }
+  }
 }
