4790b3bba6474de354e8541f74ec416d932aed33
Kasper Hesse <47347446+KasperHesse@users.noreply.github.com>
2022-01-15 20:32:46 +0100

    Add verilog generation in Top

--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -11,26 +11,21 @@ import scala.io.Source
 
 object Top extends App {
   //Initialize starting values in memory for synthesis
-//  val wordsPerBank = SynthesisMemInit("src/resources/meminit_synth")
-//
-//  //Assemble the program
+  val wordsPerBank = SynthesisMemInit("src/resources/meminit_synth")
+
+  //Assemble the program
   val source = Source.fromFile("src/resources/programs/top.txt")
   Assembler.writeMemInitFile("src/resources/programs/top.hex.txt", Assembler.assemble(source), 8)
   source.close()
-//
-//  utils.Config.INLINE = true
-//  (new chisel3.stage.ChiselStage).execute(
-//    Array("-X", "verilog", "-td", "target/gen"),
-//    Seq(ChiselGeneratorAnnotation(() => new TopLevel(
-//      IMsize=1024,
-//      IMinitFileLocation = "src/resources/programs/top.hex.txt",
-//      wordsPerBank,
-//      memInitFileLocation = "src/resources/meminit_synth",
-//      clkFreq=50e6.toInt)
-//    )))
 
-//  (new chisel3.stage.ChiselStage).execute(
-//    Array("-X", "verilog", "-td", "target/gen"), Seq(ChiselGeneratorAnnotation(() => new UartTransmitter()))
-//  )
-//  -X verilog outputs verilog, -td target/gen sets target directory
+  utils.Config.INLINE = true
+  (new chisel3.stage.ChiselStage).execute(
+    Array("-X", "verilog", "-td", "target/gen"),
+    Seq(ChiselGeneratorAnnotation(() => new TopLevel(
+      IMsize=1024,
+      IMinitFileLocation = "src/resources/programs/top.hex.txt",
+      wordsPerBank,
+      memInitFileLocation = "src/resources/meminit_synth",
+      clkFreq=50e6.toInt)
+    )))
 }
